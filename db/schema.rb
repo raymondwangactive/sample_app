120c1d78405652cc6f885dfa3a0610a4043d41ec
Ray Wang <raymond.wang@activeenergy.ca>
2012-06-07 16:58:11 -0400

    schema, index

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,6 @@ ActiveRecord::Schema.define(:version => 20120606023325) do
     t.string   "password_digest"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+
 end
