f5cb6af3c2eb44862e8074ed4941041fd38f3d4b
Raymond Wang <raymond.wang@activeenergy.ca>
2012-06-10 22:47:16 -0400

    session etc

--- config/routes.rb
+++ config/routes.rb
@@ -66,4 +66,8 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
 end
